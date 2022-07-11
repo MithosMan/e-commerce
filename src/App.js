a41de71c79fa255d6bbe612458346236cdbd35ca
MithosMan <99754781+MithosMan@users.noreply.github.com>
2022-07-11 00:11:27 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navegacion from './components/NavBar';
+import ListContainer from './components/ItemListContainer';
 
 function App() {
   return (
     <div className="App">
     <Navegacion />
+    <ListContainer />
     </div>
   );
 }
